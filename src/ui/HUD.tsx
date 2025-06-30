4284389bdd9bffc7ec94b1f81ae23b69a3a21faf
Catherine Sangiovanni <catherine.sangiovanni@gmail.com>
2025-06-29 22:32:20 -0400

    updated hud to match new format

--- src/ui/HUD.tsx
+++ src/ui/HUD.tsx
@@ -4,44 +4,32 @@ import { FPS } from '../core/Game'
 
 export default function HUD() {
     const [fps, setFps] = useState(FPS.data)
-    const [position, setPosition] = useState(renderer.Camera.position.data)
-    const [rotation, setRotation] = useState(renderer.Camera.rotation.data)
-    const [pitch, setPitch] = useState(renderer.Camera.pitch.data)
     const [drawCalls, setDrawCalls] = useState(renderer.draws.data)
+    const [cameraTransform, setCameraTransform] = useState(
+        renderer.Camera.transform.data
+    )
 
     useEffect(() => {
         const fpsListener = (newFps: typeof FPS.data) => {
             setFps(newFps)
         }
-        const positionListener = (
-            newPosition: typeof renderer.Camera.position.data
-        ) => {
-            setPosition(newPosition)
-        }
-        const rotationListener = (
-            newRotation: typeof renderer.Camera.rotation.data
-        ) => {
-            setRotation(newRotation)
-        }
-        const pitchListener = (newPitch: typeof renderer.Camera.pitch.data) => {
-            setPitch(newPitch)
-        }
         const drawsListener = (newDraws: typeof renderer.draws.data) => {
             setDrawCalls(newDraws)
         }
+        const cameraTransformListener = (
+            newTransform: typeof renderer.Camera.transform.data
+        ) => {
+            setCameraTransform(newTransform)
+        }
 
         FPS.addListener(fpsListener)
-        renderer.Camera.position.addListener(positionListener)
-        renderer.Camera.rotation.addListener(rotationListener)
-        renderer.Camera.pitch.addListener(pitchListener)
         renderer.draws.addListener(drawsListener)
+        renderer.Camera.transform.addListener(cameraTransformListener)
 
         return () => {
             FPS.removeListener(fpsListener)
-            renderer.Camera.position.removeListener(positionListener)
-            renderer.Camera.rotation.removeListener(rotationListener)
-            renderer.Camera.pitch.removeListener(pitchListener)
             renderer.draws.removeListener(drawsListener)
+            renderer.Camera.transform.removeListener(cameraTransformListener)
         }
     }, [])
 
@@ -49,10 +37,11 @@ export default function HUD() {
         <div className="text-white">
             <div>FPS: {fps}</div>
             <div>
-                Position: ({position.x}, {position.y}, {position.z})
+                Position: ({cameraTransform.position.x},{' '}
+                {cameraTransform.position.y}, {cameraTransform.position.z})
             </div>
-            <div>Rotation: {rotation}°</div>
-            <div>Pitch: {pitch}°</div>
+            <div>Rotation: {cameraTransform.rotation}°</div>
+            <div>Pitch: {cameraTransform.pitch}°</div>
             <div>Draw Calls: {drawCalls}</div>
         </div>
     )
